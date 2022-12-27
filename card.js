e48674f7bcf3d73988a3b1766caa2fcac52b745b
Aleksandra <brankovan.aleksandra@gmail.com>
2022-12-27 09:39:54 +0100

    second-commit

--- card.js
+++ card.js
@@ -1,13 +1,15 @@
 
 let rateNumber='';
 
-function changeText(message) {
-     rateNumber=message; 
+function changeText(event, message) {
+    let buttons=document.getElementsByClassName('singleButton');
+    for (let item of buttons) {
+        item.style.backgroundColor = '#2e3742';
+    }
+    event.srcElement.style.backgroundColor = "hsl(216, 12%, 54%)";
+    rateNumber=message; 
 }
 
 function submit(){
     submitNumber(`You selected ${rateNumber} from 5`);
-}
-function changeColor(button){
-    button.style.backgroungColor = "red";
 }
